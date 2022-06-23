f7215163f80314c7fd2c5b846256a8ae36409c0f
AmaniMagdoubi <amanimagdoubi@gmail.com>
2022-06-23 14:50:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const diceAudio = document.getElementsByTagName("audio"); 
+const buttonClick =document.getElementsByTagName("button")
+
+let runningScore = 0; 
+let targetScore = 20; 
+let loseValue = 1; 
+
+// rollDice Function and generates a random number betwwen 1 - 6 
+const rollDice = () => {
+    diceValue = console.log(Math.floor(Math.random() * 6 + 1));
+    diceAudio.play(); 
+}
+
+buttonClick.addEventListener("click", 
+while(gameRunning == true) {
+    if (runningScore >= 20) {
+        
+    }
+
+});  
+
+document.getElementById("demo").addEventListener("click", myFunction);
+
+
+
+
+
+
+
+
